ffd19a9f8e6e4e4d22b0a32f1ee75bb2b83775e0
Edgar2177 <74991515+Edgar2177@users.noreply.github.com>
2026-04-01 20:09:36 -0600

    Cambios
    Inevntory, Orders, PhysicalI, products y email

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Importar rutas
 const productTypesRoutes = require('./src/routes/productTypes');
